c3b44294dd333157c735800150f986cb59bbb7fa
wanghao <117663444@qq.com>
2016-05-25 06:52:32 +0800

    播放器的配置选项

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
 (function() {
   "use strict";
 
-  plyr.setup();
+  plyr.setup({
+    // enabled: false,
+    // autoplay: true,
+    // debug: true,
+    // captions: {
+    //   defaultActive: true
+    // },
+    // tooltips: {
+    //   seek: true,
+    //   controls: true,
+    // }
+  });
 
 }());
